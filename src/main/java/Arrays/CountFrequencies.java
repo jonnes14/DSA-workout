334786531c5e050ea8c2c9f85b7872782eadc246
Jonnes <jonnes400@gmail.com>
2025-07-15 11:32:32 +0530

    all changes

---
+++ src/main/java/Arrays/CountFrequencies.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+import java.util.*;
+
+class CountFrequencies{
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("");
+        int n=s.nextInt();
+        int  arr[]=new int[n];
+
+        System.out.println("");
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+            else{
+                map.put(arr[i],1);
+            }
+        }
+        for(Map.Entry<Integer,Integer>i: map.entrySet()){
+            System.out.println(i.getKey()+" "+i.getValue());
+        }
+
+    }
+}
