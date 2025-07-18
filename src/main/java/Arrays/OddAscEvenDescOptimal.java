c6370e13250e766e34df874d6be472bebb3fefe6
Jonnes <jonnes400@gmail.com>
2025-07-18 23:55:12 +0530

    sort

---
+++ src/main/java/Arrays/OddAscEvenDescOptimal.java
@@ -0,0 +1,53 @@
+package Arrays;
+
+import java.util.*;
+
+public class OddAscEvenDescOptimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] even = new int[n];
+        int[] odd = new int[n];
+        int evenCount = 0, oddCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] % 2 == 0) {
+                even[evenCount++] = arr[i];
+            } else {
+                odd[oddCount++] = arr[i];
+            }
+        }
+
+
+        for (int i = 0; i < evenCount; i++) {
+            for (int j = i + 1; j < evenCount; j++) {
+                if (even[i] < even[j]) {
+                    int temp = even[i];
+                    even[i] = even[j];
+                    even[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < oddCount; i++) {
+            for (int j = i + 1; j < oddCount; j++) {
+                if (odd[i] > odd[j]) {
+                    int temp = odd[i];
+                    odd[i] = odd[j];
+                    odd[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < evenCount; i++) {
+            System.out.print(even[i] + " ");
+        }
+
+        for (int i = 0; i < oddCount; i++) {
+            System.out.print(odd[i] + " ");
+        }
+    }
+}
