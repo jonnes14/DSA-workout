60d926c8e4f4b7477eef73bca5e5262af6393cc8
Jonnes <jonnes400@gmail.com>
2025-07-29 23:28:24 +0530

    working on main class

--- src/main/java/AdvanceProgramming/RailwayReservation/RailwayCode.java
+++ src/main/java/AdvanceProgramming/RailwayReservation/RailwayCode.java
@@ -10,7 +10,7 @@ public class RailwayCode {
     private final List<Integer> lower=new ArrayList<>();
     private final List<Integer> upper=new ArrayList<>();
     private final List<Integer> middle=new ArrayList<>();
-
+    private final List<Integer> sidelower=new ArrayList<>();
     public void BookTicket(String name, int age,String gender,String berthPreference) {
         Passenger passenger;
         if(!seatbirth.isEmpty()) {
@@ -19,7 +19,7 @@ public class RailwayCode {
         }
 
     }
-    public String AllocateBirth(String name,int age, String preference, String gender, boolean hasChild, boolean israc) {
+    public String BirthCondition(String name,int age, String preference, String gender, boolean hasChild, boolean israc) {
         if(age<5) {
             booking.add(name+","+age+","+gender);
             return "age is less than 5 no seat allocated";
@@ -38,7 +38,13 @@ public class RailwayCode {
         return seatbirth.get(0);
     }
     public void tickets() {
-        for(int i=0;i<)
+        for(int i=0;i<21;i++) lower.add(i);
+        for(int i=0;i<21;i++) middle.add(i);
+        for(int i=0;i<21;i++) lower.add(i);
+        for(int i=0;i<9;i++) sidelower.add(i);
+    }
+    public void cancelTicket(){
+
     }
 
 }
---
+++ src/main/java/AdvanceProgramming/RailwayReservation/TicketBooking.java
@@ -0,0 +1,35 @@
+package AdvanceProgramming.RailwayReservation;
+
+import java.util.Scanner;
+
+public class TicketBooking {
+    public static void main(String[] args) {
+        RailwayCode r=new RailwayCode();
+        Scanner s =new Scanner(System.in);
+        while(true) {
+            System.out.println("Railway Reservation");
+            System.out.println("Enter 1 for Book");
+            System.out.println("Enter 2 for Cancel");
+            System.out.println("Enter 3 for Print booked tickets");
+            System.out.println("Enter 4 for Print available tickets (details with summary)");
+            System.out.println("Enter 5 for Exit");
+            System.out.println("Enter your choice");
+            int choice=s.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter name");
+                    String name=s.nextLine();
+                    System.out.println("Enter age");
+                    int age=s.nextInt();
+                    System.out.println("Enter Gender");
+                    String gender=s.nextLine();
+                    System.out.println("Enter Berth preference (L/U/M)");
+                    String berthpreference=s.nextLine();
+                    r.BookTicket(name,age,gender,berthpreference);
+                    break;
+
+            }
+        }
+
+    }
+}
